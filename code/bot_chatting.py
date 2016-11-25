faeb3f071e974a04abedb8110d0bb21c5bf6b862
TryHarder01 <randyclinton@gmail.com>
2016-11-25 16:58:58 -0500

    code skeltons

---
+++ code/bot_chatting.py
@@ -0,0 +1,8 @@
+"""
+The conversation logic to get the information from users
+"""
+
+introduction = "Hello {}, before we start let me check if we've done this before..."
+
+new= """ok let's do this. I'm going to ask you for three things you like and three things you don't like. Once everyone has done that, I'll use fancy computer things
+to figure things out"""
